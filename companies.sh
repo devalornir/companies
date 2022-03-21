037b98827b0ee86c0cae26f2da476f09348f031d
devalornir <or.nir@devalore.com>
2022-03-21 14:27:05 +0200

    get company data

--- companies.sh
+++ companies.sh
@@ -3,4 +3,4 @@
 read -p "Company Name: " company_name
 url='https://company.clearbit.com/v2/companies/find?domain='
 URL_TOKEN='sk_30240e2d1dfc1d73d26ab80390d1fd49'
-curl ${url}"[${company_name}]" -H "Accept: application/json" -H "Authorization: Bearer ${URL_TOKEN}"
+curl ${url}${company_name} -H "Accept: application/json" -H "Authorization: Bearer ${URL_TOKEN}"
